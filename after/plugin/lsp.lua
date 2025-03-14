81af69e95ab17b293956633eb591620b3ec02590
kramwani <kapil@appperfect.com>
2025-03-15 02:53:29 +0530

    fix(harpoon): fixing deprecated lua method

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,15 +3,16 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-    'tsserver',
-    'rust_analyzer',
-    'gopls',
-    'lua_ls',
-    'terraformls',
-    'tflint',
-    'pyright',
-    'ruff_lsp',
-    'bashls'
+  'tsserver',
+  'rust_analyzer',
+  'gopls',
+  'lua_ls',
+  'terraformls',
+  'tflint',
+  'pyright',
+  'ruff_lsp',
+  'bashls',
+  'clangd'
 })
 
 -- Fix Undefined global 'vim'
@@ -20,96 +21,96 @@ lsp.nvim_workspace()
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['C-p'] = cmp.mapping.select_prev_item(cmp_select),
-    ['C-n'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
+  ['C-p'] = cmp.mapping.select_prev_item(cmp_select),
+  ['C-n'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
 })
 
 cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
+  mapping = cmp_mappings
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+  suggest_lsp_servers = false,
+  sign_icons = {
+    error = 'E',
+    warn = 'W',
+    hint = 'H',
+    info = 'I'
+  }
 })
 
 local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
 -- Prettier Autoformat for JSON, YAML, Markdown
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = { "*.json", "*.yaml", "*.yml", "*.md" },
-    callback = function()
-        vim.cmd("silent! PrettierAsync") -- Assuming you have Prettier set up as a plugin or external command
-    end,
+  pattern = { "*.json", "*.yaml", "*.yml", "*.md" },
+  callback = function()
+    vim.cmd("silent! PrettierAsync") -- Assuming you have Prettier set up as a plugin or external command
+  end,
 })
 
 
 -- File-specific indentation rules
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "python",
-    callback = function()
-        vim.opt_local.shiftwidth = 4
-        vim.opt_local.tabstop = 4
-        vim.opt_local.expandtab = true
-    end,
+  pattern = "python",
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.expandtab = true
+  end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "json",
-    callback = function()
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.tabstop = 2
-        vim.opt_local.expandtab = true
-    end,
+  pattern = "json",
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.expandtab = true
+  end,
 })
 
 -- Trailing Whitespace and EOL handling
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function()
-        vim.cmd("%s/\\s\\+$//e") -- Remove trailing whitespace
-    end,
+  pattern = "*",
+  callback = function()
+    vim.cmd("%s/\\s\\+$//e") -- Remove trailing whitespace
+  end,
 })
 
 local lsp_format_on_save = function(bufnr)
-    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-    vim.api.nvim_create_autocmd('BufWritePre', {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-            vim.lsp.buf.format()
-        end,
-    })
+  vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    group = augroup,
+    buffer = bufnr,
+    callback = function()
+      vim.lsp.buf.format()
+    end,
+  })
 end
 
 lsp.on_attach(function(client, bufnr)
-    lsp_format_on_save(bufnr)
-    local opts = { buffer = bufnr, remap = false }
+  lsp_format_on_save(bufnr)
+  local opts = { buffer = bufnr, remap = false }
 
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true
+  virtual_text = true
 })
--- lua/codeknight_config/init.lua
+++ lua/codeknight_config/init.lua
@@ -2,24 +2,24 @@
 require("codeknight_config.remap")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("codeknight_config.plugins")
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
-vim.o.tabstop = 4      -- A TAB character looks like 4 spaces
+vim.o.tabstop = 2      -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4  -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4   -- Number of spaces inserted when indenting
+vim.o.softtabstop = 2  -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 2   -- Number of spaces inserted when indenting
 vim.opt.spelllang = 'en_us'
 vim.opt.spell = true
 vim.opt.foldmethod = "expr"
@@ -28,7 +28,7 @@ vim.opt.foldcolumn = "0"
 vim.opt.foldtext = ""
 vim.opt.foldlevel = 99
 vim.diagnostic.config({
-    virtual_text = false
+  virtual_text = false
 })
 
 -- Show line diagnostics automatically in hover window
@@ -40,5 +40,8 @@ vim.cmd [[
     autocmd BufWritePost,BufEnter *.txt setlocal autoread
     autocmd TextChanged,TextChangedI *.txt silent! write
 ]]
+
+vim.cmd("command! W write")
+
 
 print("hello from codeknight")
